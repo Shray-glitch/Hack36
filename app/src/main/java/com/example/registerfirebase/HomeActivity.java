c5ed6821e0346839f4ecb3e47ad5408b8e26f870
sonalprd <sonal.prasad.01@gmail.com>
2021-04-10 15:53:13 +0530

    banner slider

--- app/src/main/java/com/example/registerfirebase/HomeActivity.java
+++ app/src/main/java/com/example/registerfirebase/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.registerfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
@@ -16,17 +18,32 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     Button btnProduct;
 
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int currentPage;
+
+
+    private Timer timer;
+    final private long DELAY_TIME=3000;
+    final private long PERIOD_TIME=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        bannerSliderViewPager=findViewById(R.id.banner_slider_view_pager);
         setSupportActionBar(toolbar);
      //   FirebaseAuth.getInstance().signOut();
 
@@ -63,7 +80,53 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        sliderModelList =new ArrayList<SliderModel>();
 
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher_round));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+
+
+
+        SliderAdapter sliderAdapter=new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+         ViewPager.OnPageChangeListener onPageChangeListener=new ViewPager.OnPageChangeListener() {
+             @Override
+             public void onPageScrolled(int i, float positionOffset, int positionOffsetPixels) {
+
+             }
+
+             @Override
+             public void onPageSelected(int i) {
+                 currentPage=i;
+             }
+
+             @Override
+             public void onPageScrollStateChanged(int i) {
+                 if(i==ViewPager.SCROLL_STATE_IDLE)
+                     PageLooper();
+             }
+         };
+         bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+         startBannerSlideShow();
+
+         bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+             @Override
+             public boolean onTouch(View v, MotionEvent event) {
+                 PageLooper();
+                 stopSlideShow();
+                 if(event.getAction()==MotionEvent.ACTION_UP)
+                 {
+                     startBannerSlideShow();
+                 }
+                 return false;
+             }
+         });
     }
 
     @Override
@@ -78,5 +141,46 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    private void PageLooper()
+    {
+        if(currentPage==sliderModelList.size()-2)
+        {
+            currentPage=2;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+        if(currentPage==1)
+        {
+            currentPage=sliderModelList.size()-3;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+    }
+
+    private void startBannerSlideShow()
+    {
+        Handler handler=new Handler();
+        Runnable update=new Runnable() {
+            @Override
+            public void run() {
+                if(currentPage>=sliderModelList.size())
+                {
+                    currentPage=1;
+                }
+                bannerSliderViewPager.setCurrentItem(currentPage++,true);
+            }
+        };
+
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },DELAY_TIME,PERIOD_TIME);
+    }
+
+    private void stopSlideShow()
+    {
+        timer.cancel();
     }
 }
